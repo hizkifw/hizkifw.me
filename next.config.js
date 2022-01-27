d3e31ff1509f52d042c07a8d2201809660351ff9
Hizkia Felix Winata <felix@hizkifw.me>
2022-01-28 04:03:14 +0800

    feat: add openpgp key

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/.well-known/openpgpkey/hu/:keyid',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/octet-stream',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+};
